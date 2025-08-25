d149af603bf70e165be37f8ca170948b840ac778
Treashep <3282407418@qq.com>
2025-08-25 17:21:27 +0800

    加入cors

--- psyas/extensions.py
+++ psyas/extensions.py
@@ -2,12 +2,12 @@
 """Extensions module. Each extension is initialized in the app factory located in app.py."""
 from flask_bcrypt import Bcrypt
 from flask_caching import Cache
+from flask_cors import CORS
 from flask_debugtoolbar import DebugToolbarExtension
 from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_static_digest import FlaskStaticDigest
-from flask_cors import CORS
 
 bcrypt = Bcrypt()
 cors = CORS()
