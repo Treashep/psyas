ef5f11af22e4951344c335a0c1fdd68cb6bbf669
Treashep <3282407418@qq.com>
2025-08-25 16:48:39 +0800

    修改扩展文件

--- psyas/extensions.py
+++ psyas/extensions.py
@@ -7,10 +7,10 @@ from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_static_digest import FlaskStaticDigest
-from flask_wtf.csrf import CSRFProtect
+from flask_cors import CORS
 
 bcrypt = Bcrypt()
-csrf_protect = CSRFProtect()
+cors = CORS()
 login_manager = LoginManager()
 db = SQLAlchemy()
 migrate = Migrate()
