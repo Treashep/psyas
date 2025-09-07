d451dfbbe2bc70d0638faf323c69c3e07addeadc
mix <3423136141@qq.com>
2025-09-08 00:09:01 +0800

    添加 login 样式与页面结构，新增导航栏

---
+++ front/src/pages/bar/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '/src/pages/bar/index.css';
+
+function Bar() {
+  return (
+    <nav className="nav">
+      <div className="logo"><a href="#">心理聊天助手</a></div>
+    </nav>
+  );
+}
+
+export default Bar
--- front/src/pages/login/index.jsx
+++ front/src/pages/login/index.jsx
@@ -1,5 +1,24 @@
+import Bar from "../bar"
+import '/src/pages/login/index.css';
 const Login = ()=>{
-  return <div>this is login</div>
+  return (
+    <div className="body">
+      <Bar />
+      <div className="left">
+        <p>
+          那些知道为了什么而活的人几乎可以承受任何磨难；
+        </p>
+        <p>
+          谁懂得了为什么生活，谁就能承受任何一种生活。
+        </p>
+        <p className="people">
+          ————尼采
+        </p>
+      </div>
+      <div className="right">22</div>
+    </div>
+  )
+  
 }
 
 export default Login
