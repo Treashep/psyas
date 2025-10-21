1b7511fa92b416f68558566918a675fefa1b75f7
mix <3423136141@qq.com>
2025-10-21 16:22:15 +0800

    更新路由文件

--- front/src/router/index.jsx
+++ front/src/router/index.jsx
@@ -2,19 +2,32 @@ import { createBrowserRouter } from "react-router-dom";
 import Talk from "../pages/talk/index.jsx";
 import Login from "../pages/login/index.jsx"; // 修正路径
 import Register from "../pages/register/index.jsx";
+import { RootLayout, ProtectedRoute } from "../components/RootLayout";
 
 const router = createBrowserRouter([
   {
-    path: '/talk',
-    element: <Talk></Talk>,
-  },
-  {
     path: '/',
-    element: <Login></Login>,
-  },
-  {
-    path: '/register',
-    element: <Register></Register>,
+    element: <RootLayout />,
+    children: [
+      {
+        path: '/talk',
+        element: <ProtectedRoute />,
+        children: [
+          {
+            index: true,
+            element: <Talk></Talk>,
+          }
+        ]
+      },
+      {
+        path: '/',
+        element: <Login></Login>,
+      },
+      {
+        path: '/register',
+        element: <Register></Register>,
+      },
+    ]
   },
 ]);
 
