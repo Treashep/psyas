c5742c5eda4458320764f7c1707bc0649155036f
mix <3423136141@qq.com>
2025-10-21 16:24:00 +0800

    更新全局背景

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -5,10 +5,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import router from './router/index.jsx'
 import store from './store'
+import { HelmetProvider } from 'react-helmet-async'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <Provider store={store}>
-    <RouterProvider router={router} />
+    <HelmetProvider>
+      <RouterProvider router={router} />
+    </HelmetProvider>
   </Provider>
 )
